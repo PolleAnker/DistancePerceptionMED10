14fe312cb89716335116fd0a0a4590eff963f5bc
Morten Trier Bach <36537080+MortenBachJ@users.noreply.github.com>
2022-02-16 13:26:58 +0100

    Ball Roll is in!

---
+++ Assets/Scripts/BallRoller.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoller : MonoBehaviour
+{
+    public GameObject startPoint;
+    public GameObject ball;
+    private Rigidbody rb;
+    private float ballRollDistance;
+
+    [Header("Press this to reset the ball")]
+    public bool resetBall = false;
+
+    [Header("Select Force")]
+    [Range(0f,9f)]public float force = 0f;    
+
+    [Header("Press this to roll the ball")]
+    public bool pushBall = false;
+
+    [Header("Press this to print distance to the ball")]
+    public bool distance = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = ball.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(resetBall) {
+            resetBall = false;
+            ball.transform.position = startPoint.transform.position;
+        }
+
+        if(distance){
+            distance = false;
+            if(ball){
+                ballRollDistance = Vector3.Distance(startPoint.transform.position, ball.transform.position);
+                print("The ball rolled a distance of " + ballRollDistance + " meters");
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(pushBall) {
+            pushBall = false;
+            rb.AddForce(0, 0, force, ForceMode.Impulse);
+        }
+    }
+}
