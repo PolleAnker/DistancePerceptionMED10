2c02839d13f72417d14ab1e92cb00f27496cf3ba
PolleAnker <thementoryay@gmail.com>
2022-02-17 14:12:34 +0100

    Changes to start tower

--- Assets/Scripts/WireScaler.cs
+++ Assets/Scripts/WireScaler.cs
@@ -4,42 +4,68 @@ using UnityEngine;
 
 public class WireScaler : MonoBehaviour
 {
-    public Transform startPoint;
+    public GameObject startTowerPrefab;
+    private GameObject startPoint;
+    public bool spawnStartTower = false;
     public Transform endPoint;
     private Vector3 initialScale;
     private float distBetweenPoints;
+    public Transform mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         initialScale = transform.localScale;
         //print("Scaling object in start method");
-        RescaleObject();
+        if(startPoint){
+            RescaleObject();
+        }
+    }
+    void Awake()
+    {
+        startPoint = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Only do rescaling if either of the two points to scale between has changed
-        if(startPoint.transform.hasChanged || endPoint.transform.hasChanged)
-        {
-            //print("Rescaling object");
-            RescaleObject();
+        if(spawnStartTower){
+            spawnStartTower = false;
+            SpawnStartTower();
+        }
+        
+        if(startPoint != null){
+            // Only do rescaling if either of the two points to scale between has changed
+            if(startPoint.transform.hasChanged || endPoint.transform.hasChanged)
+            {
+                //print("Rescaling object");
+                RescaleObject();
+            }
         }
     }
 
     private void RescaleObject()
     {
         // Calculate the scale of the object
-        distBetweenPoints = Vector3.Distance(startPoint.position, endPoint.position);
+        distBetweenPoints = Vector3.Distance(startPoint.transform.position, endPoint.position);
         transform.localScale = new Vector3(initialScale.x, distBetweenPoints/2f, initialScale.z);   // Division by 2 for a cylinder object
 
         // Calculate the position to place this object at
-        Vector3 middlePoint = (startPoint.position + endPoint.position) / 2;    // find middle between the two points (where to put this object)
+        Vector3 middlePoint = (startPoint.transform.position + endPoint.position) / 2;    // find middle between the two points (where to put this object)
         transform.position = middlePoint;
 
         // Calculate how to rotate / where to face this object
-        Vector3 rotationDirection = (endPoint.position - startPoint.position);
+        Vector3 rotationDirection = (endPoint.position - startPoint.transform.position);
         transform.up = rotationDirection;
+    }
+    private void SpawnStartTower(){
+        if(!startPoint){
+            Vector3 spawnPosition = new Vector3(mainCamera.position.x, 0.325f, mainCamera.position.z);
+            startPoint = Instantiate(startTowerPrefab, spawnPosition, Quaternion.identity);
+        }
+        else{
+            Vector3 spawnPosition = new Vector3(mainCamera.position.x, 0.325f, mainCamera.position.z);
+            startPoint.transform.position = spawnPosition;
+        }
     }
 }
